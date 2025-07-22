ed01723650a77c1c3bebd31ab900e804d9972117
codedbydumi <duminduthushan9@gmail.com>
2025-07-23 00:12:41 +0530

    .
    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { SpeedInsights } from '@vercel/speed-insights/react'
-import { Analytics } from '@vercel/analytics/next';
+
 import App from './App.tsx'
 import './index.css'
 
@@ -8,6 +8,6 @@ createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <SpeedInsights />
-    <Analytics />
+  
   </>
 );
